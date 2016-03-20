cfcf82049cf11cc131b6bf045eb942c9ac275c64
Brandon Kallaher <brandonj@kallaher.org>
2016-03-20 13:12:48 -0700

    added launch files and updated .gitignore

--- scripts/location_optitrack.py
+++ scripts/location_optitrack.py
@@ -12,17 +12,17 @@ from gazebo_msgs.msg import ModelStates
 
 class Location:
     def __init__(self):
-        self.location_sub = rospy.Subscriber('/vrpn_client_node/ardrone/pose', PoseStamped, self.location_callback)
+        self.location_sub = rospy.Subscriber('/vrpn_client_node/robot/pose', PoseStamped, self.location_callback)
         #create message publisher for sending drone movement commands
-        self.output_pub = rospy.Publisher('/ardrone/location', Pose, queue_size=1)
+        self.output_pub = rospy.Publisher('/robot/location', Pose, queue_size=1)
         self.count = 0
 
     def location_callback(self, location):
         #gazebo publishes at 100hz, only publish every 5th message to get 20hz
         self.count += 1
         if self.count > 4:
-            quadrotor_pose = location.pose
-            self.output_pub.publish(quadrotor_pose)
+            robot_pose = location.pose
+            self.output_pub.publish(robot_pose)
             self.count = 0
 
 
